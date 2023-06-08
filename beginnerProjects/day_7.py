7bace8b21625ac9d629d988fb7d666f9efc43e5c
ErhNay <erhnay86@gmail.com>
2023-06-08 22:21:47 +0000

    check if player won

--- beginnerProjects/day_7.py
+++ beginnerProjects/day_7.py
@@ -16,8 +16,13 @@ word_length = len(chosen_word)
 for letter in chosen_word:
     display.append('_')
 print(display)
-guess = input("Please guess a letter: ").lower()
-print(f"You chose: {guess}")
+
+# Use a while loop to let the user guess again.
+# The loop should only stop once the user has guessed all the letters in the chosen_word
+# and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+end_of_game = False
+while not end_of_game:
+    guess = input("Please guess a letter: ").lower()
 # Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 # //Alternative
 # for letter in chosen_word:
@@ -28,13 +33,14 @@ print(f"You chose: {guess}")
 # Loop through each position in the chosen_word;
 # If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 # e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for position in range(0, word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-    else:
-        print("Wrong")
+    for position in range(0, word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
 # Print 'display' and you should see the guessed letter in the correct position and every other letter replaced with
 # "_". Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-print(display)
+    print(display)
+    if '_' not in display:
+        end_of_game = True
+        print("You win!")
 
