27ed38437694f7d83bf0050fe7fac6ec05c2c8d2
ErhNay <erhnay86@gmail.com>
2023-06-09 19:05:44 +0000

    prime number checker

--- beginnerProjects/day_8.py
+++ beginnerProjects/day_8.py
@@ -30,18 +30,33 @@
 # greet_with(location="Russia", name="Norbert")
 
 
-
 # Exercise 1 Paint Area Calculator
-import math
+# import math
+#
+#
+# def paint_calc(height, width, cover):
+#     area = height * width
+#     number_of_cans = math.ceil(area / cover)
+#     print(f"You'll need {number_of_cans} cans of paint")
+#
+#
+# test_h = int(input("Height of wall: "))
+# test_w = int(input("Width of wall: "))
+# coverage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverage)
+
+# Exercise 2 Prime Number Checker
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+            print(i)
+    if is_prime:
+        print("It's a prime number")
+    else:
+        print("It's not a prime number")
 
 
-def paint_calc(height, width, cover):
-    area = height * width
-    number_of_cans = math.ceil(area / cover)
-    print(f"You'll need {number_of_cans} cans of paint")
-
-
-test_h = int(input("Height of wall: "))
-test_w = int(input("Width of wall: "))
-coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
+n = int(input("Check this number: "))
+prime_checker(number=n)
