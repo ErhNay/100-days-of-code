d772c2784fbacd7f087ca37ea6e02648eb6c399f
ErhNay <erhnay86@gmail.com>
2023-06-10 00:59:46 +0000

    Caesar Cipher, encryption part

--- beginnerProjects/day_8.py
+++ beginnerProjects/day_8.py
@@ -46,17 +46,52 @@
 # paint_calc(height=test_h, width=test_w, cover=coverage)
 
 # Exercise 2 Prime Number Checker
-def prime_checker(number):
-    is_prime = True
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-            print(i)
-    if is_prime:
-        print("It's a prime number")
-    else:
-        print("It's not a prime number")
+# def prime_checker(number):
+#     is_prime = True
+#     for i in range(2, number):
+#         if number % i == 0:
+#             is_prime = False
+#             print(i)
+#     if is_prime:
+#         print("It's a prime number")
+#     else:
+#         print("It's not a prime number")
+#
+#
+# n = int(input("Check this number: "))
+# prime_checker(number=n)
+
+# Prroject of the day  (Caesar Cipher)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 
-n = int(input("Check this number: "))
-prime_checker(number=n)
+# Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    alphabet.extend(alphabet)
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+
+
+encrypt(plain_text=text, shift_amount=shift)
+# Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+# e.g.
+# plain_text = "hello"
+# shift = 5
+# cipher_text = "mjqqt"
+# print output: "The encoded text is mjqqt"
+
+# HINT: How do you get the index of an item in a list:
+# https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+# Bug alert: What happens if you try to encode the word 'civilization'?🐛
