59435646f95e4ef2de7eb61b8a2c16fcbc62513c
ErhNay <erhnay86@gmail.com>
2023-06-10 11:51:38 +0000

    code refactoring

--- beginnerProjects/day_8.py
+++ beginnerProjects/day_8.py
@@ -71,33 +71,18 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text, shift_amount):
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
     alphabet.extend(alphabet)
-    cipher_text = ""
-    for letter in plain_text:
+    for letter in start_text:
         position = alphabet.index(letter)
+        if cipher_direction == "decode":
+            shift_amount *= -1
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+        end_text += alphabet[new_position]
+        shift_amount = 5
+    print(f"The {cipher_direction}d text is {end_text}")
 
+#  if cipher_direction == "decode": shift_amount *= -1 can be placed above the for in loop
 
-def decrypt(cipher_text, shift_amount):
-    alphabet.extend(alphabet)
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        plain_text += new_letter
-    print(f"The decoded text is {plain_text}")
-
-
-if direction == 'encode':
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == 'decode':
-    decrypt(cipher_text=text, shift_amount=shift)
-
-
-
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
