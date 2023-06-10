f154ed4720fdaacb08d58824398f40b2f226ee52
ErhNay <erhnay86@gmail.com>
2023-06-10 17:16:39 +0000

    Caesar Cipher final touches

--- beginnerProjects/day_8.py
+++ beginnerProjects/day_8.py
@@ -61,28 +61,47 @@
 # n = int(input("Check this number: "))
 # prime_checker(number=n)
 
-# Prroject of the day  (Caesar Cipher)
+# Project of the day  (Caesar Cipher)
+# from art import logo
+import art
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+logo = art.logo
+print(logo)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
+# TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+# Try running the program and entering a shift number of 45.
+# Add some code so that the program continues to work even if the user enters a shift number greater than 26.
+# Hint: Think about how you can use the modulus (%).
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
-    alphabet.extend(alphabet)
-    for letter in start_text:
-        position = alphabet.index(letter)
-        if cipher_direction == "decode":
-            shift_amount *= -1
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
-        shift_amount = 5
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
     print(f"The {cipher_direction}d text is {end_text}")
 
-#  if cipher_direction == "decode": shift_amount *= -1 can be placed above the for in loop
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'\n").lower()
+    if result == 'no':
+        should_continue = False
+        print("Goodbye...")
